9e9614f6e65bf935bd8d8a85f765f2c56c5a46b8
suwidnyana <grayfullbuster522@gmail.com>
2025-11-21 11:20:48 +0800

    agar bisa jalan di railway

--- app.py
+++ app.py
@@ -154,5 +154,9 @@ def about():
     return render_template("about.html")
 
 
+# ----------------------------------------------------
+# LOCAL RUN ONLY
+# ----------------------------------------------------
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
